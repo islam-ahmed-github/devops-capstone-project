e0661591b90ed0b811603c0fa192e38e9cc576d0
islam-ahmed-github <112736634+islam-ahmed-github@users.noreply.github.com>
2022-11-27 23:17:03 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
     )
 
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -78,6 +79,7 @@ def list_accounts():
 
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
 
 
 ######################################################################
@@ -109,16 +111,15 @@ def update_accounts(account_id):
     This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     account.deserialize(request.get_json())
     account.update()
-
     return account.serialize(), status.HTTP_200_OK
  
+    
+    
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
